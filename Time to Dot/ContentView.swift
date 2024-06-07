7a5f5e33c48cb7a16c947bfe25fffa694a70f498
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-06-07 23:25:50 +0900

    Initial Commit

---
+++ Time to Dot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Time to Dot
+//
+//  Created by Jia Jang on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Time to Dot/Time_to_DotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_to_DotApp.swift
+//  Time to Dot
+//
+//  Created by Jia Jang on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Time_to_DotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

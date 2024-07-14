869acebe70fbbda932378769a0f1cd00d958ee29
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-07-14 17:32:07 +0900

    Initial Commit

---
+++ TimetoDot/TimetoDotApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetoDotApp.swift
+//  TimetoDot
+//
+//  Created by Jia Jang on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TimetoDotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
